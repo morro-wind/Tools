0482c4d3a4d2f3f476a976c14a0db5419091ef62
morro-wind <imorrowind@hotmail.com>
2017-04-11 13:06:30 +0800

    optimal start.sh

--- shell/start.sh
+++ shell/start.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-
+#
+# start - this script starts and stops the application
+#
+# chkconfig:   - 85 15
 #maintainer morro-wind mail address imorrowind#hotmail.com
 #The script all application {start|stop|restart}
 
 USER_HOME=/usr/mpsp
 
-cd $USER_HOME
-
 do_start()
 {
-    for i in `find ./ -path ./appBakeup -prune -o -path ./shengchan_bak -prune -path ./lazy -prune -path ./yunxing -prune -o -name "serv.sh" -exec dirname {} \;`
+    for i in `find $USER_HOME -path ${USER_HOME}/appBakeup -prune -o -path \
+        ${USER_HOME}/shengchan_bak -prune -path ${USER_HOME}/lazy -prune -path \
+        ${USER_HOME}/yunxing -prune -o -name "serv.sh" -exec dirname {} \;`
     do
-        cd ${USER_HOME}/$i
+        cd $i
         sed -i '/^tail/s/^/#/' serv.sh
     	./serv.sh 2>/dev/null
     done
 
-    for i in `find ./ -path ./appBakeup -prune -o -path ./shengchan_bak -prune -path ./lazy -prune -path ./yunxing -prune -o -name "run.sh" -exec dirname {} \;`
+    for i in `find ${USER_HOME}/ -path ${USER_HOME}/appBakeup -prune -o -path \
+        ${USER_HOME}/shengchan_bak -prune -path ${USER_HOME}/lazy -prune -path \
+        ${USER_HOME}/yunxing -prune -o -name "run.sh" -exec dirname {} \;`
     do
-        cd ${USER_HOME}/$i
+        cd $i
         sed -i '/^tail/s/^/#/' run.sh
         ./run.sh 2>/dev/null
     done
@@ -32,6 +37,7 @@ do_start()
 
 do_stop()
 {
+    cd ${USER_HOME}
     for i in `find ./ -path ./appBakeup -prune -o -path ./shengchan_bak -prune -path ./lazy -prune -path ./yunxing -prune -o -name "serv.sh" -exec dirname {} \;`
     do
         cd ${USER_HOME}/$i
