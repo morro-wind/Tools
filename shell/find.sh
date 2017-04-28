b2f16189b1856a7d23843cb072c05a552e133cbe
morro-wind <imorrowind@hotmail.com>
2017-04-28 18:44:00 +0800

    optimize find.sh

--- shell/find.sh
+++ shell/find.sh
@@ -3,18 +3,36 @@
 USER_HOME=/usr/mpsp
 
 
-    for i in `find $USER_HOME -path ${USER_HOME}/lost+found -prune -o -path ${USER_HOME}/appBakeup -prune -o -path \
-        ${USER_HOME}/shengchan_bak -prune -o -path ${USER_HOME}/lazy -prune -path \
-        ${USER_HOME}/yunxing -prune -o -name "serv.sh" -exec dirname {} \;`
+for i in `find $USER_HOME -path ${USER_HOME}/lost+found -prune -o \
+    -path ${USER_HOME}/appBakeup -prune -o \
+    -path ${USER_HOME}/shengchan_bak -prune -o \
+    -path ${USER_HOME}/lazy -prune -o \
+    -path ${USER_HOME}/yunxing -prune -o \
+    -path ${USER_HOME}/mpsp -prune -o \
+    -name "serv.sh" -exec dirname {} \;`
 do
-    echo $i
+    echo -e "\n"
+    echo -e "ls dirname:$i"
     ls $i
-    grep "Dflag\|SERVICE_ID\|SERVICE_NAME" $i/serv.sh
+
+    echo -e "\n"
+    if [ -f "$i/para" ];then
+        echo -e "cat $i/para:"
+        cat "$i/para"
+    elif [ -f "$i/appenv"];then
+        echo -e "cat $i/appenv:"
+        cat "$i/appenv"
+    fi
+
+    echo -e "\n"
+    echo -e "grep $i"
+    grep "Dflag\|SERVICE_ID\|SERVICE_NAME\|APP_NAME" $i/serv.sh
     #if [ -f appenv ]; then
     #grep "Dflag|SERVICE_ID" appenv
 #fi
 done
 
+echo -e "\n\n"
 #if ;then
 #    for i in 1
 #    do
@@ -22,26 +40,39 @@ done
 #    done
 #fi
 
-    for i in `find ${USER_HOME}/ -path ${USER_HOME}/lost+found -prune -o -path ${USER_HOME}/appBakeup -prune -o -path \
-        ${USER_HOME}/shengchan_bak -prune -o -path ${USER_HOME}/lazy -prune -o -path \
-        ${USER_HOME}/yunxing -prune -o -name "run.sh" -exec dirname {} \;`
+for i in `find ${USER_HOME}/ -path ${USER_HOME}/lost+found -prune -o \
+    -path ${USER_HOME}/appBakeup -prune -o \
+    -path ${USER_HOME}/shengchan_bak -prune -o \
+    -path ${USER_HOME}/lazy -prune -o \
+    -path ${USER_HOME}/yunxing -prune -o \
+    -path ${USER_HOME}/mpsp -prune -o \
+    -name "run.sh" -exec dirname {} \;`
 do
-    echo -e "ls dirname:$i \n"
+    echo -e "\n"
+    echo -e "ls dirname:$i"
     ls $i
 
-    if [ -f "$i/appenv" ];then
-        echo -e "cat $i/appenv: \n"
+    echo -e "\n"
+    if [ -f "$i/para" ];then
+        echo -e "cat $i/para:"
+        cat "$i/para"
+    elif [ -f "$i/appenv"];then
+        echo -e "cat $i/appenv:"
         cat "$i/appenv"
     fi
-    echo -e "grep $i \n"
-    grep "Dflag\|SERVICE_ID\|SERVICE_NAME" $i/run.sh
-#    grep "Dflag|SERVICE_ID" appenv
+
+    echo -e "\n"
+    echo -e "grep $i"
+    grep "Dflag\|SERVICE_ID\|SERVICE_NAME\|APP_NAME" $i/run.sh
 done
+
+
+echo -e "\n\n"
 
 if [ -d "${USER_HMOE}/run" ];then
     for i in `find ${USER_HOME}/run/ -regex ".*d$" -exec basename {} \;`
     do
         ls ${USER_HOME}/run/$i
-        grep "Dflag\|SERVICE_ID\|SERVICE_NAME" ${USER_HOME}/run/$i
+        grep "Dflag\|SERVICE_ID\|SERVICE_NAME\|APP_NAME" ${USER_HOME}/run/$i
     done
 fi
