b69bb86a5ca1e3ada81ebde889ba8ebb971ac6c3
lie <1713931181@qq.com>
2018-03-22 13:44:15 +0800

    add materials and perfect function

--- shell/sql.sh
+++ shell/sql.sh
@@ -1,33 +1,55 @@
 #!/bin/env bash
-# Version: autorun sql files 0.1
+# Version: autorun sql files 0.2
 # MAINTAINER morrowind "@qq.com"
-# set -x
+# Set -x
 # The shell that psql is to execute the given command string
 
-# set language env
+# Set language env
 export LANG=zh_CN.UTF-8
 
-# set variable
-DATE=`date +%Y%m%d`
+# Set variable
+DATE=$1
+[ ${DATE} == "" ] && DATE=`date +%Y%m%d`
 DB_NAME=""
 BACK_PATH=""
 SVN_PATH="./"
-LOG_PATH="./${DATE}.log"
+SVN_BIN=""
+SVN_USER=""
+SVN_PWD=""
+SVN_ADDR=""
+LOG_PATH="./"
 PSQL_BIN=`which psql`
 PSQL_HOST=""
 PSQL_PORT=""
 PSQL_USER=""
 PSQL_PASSWD=""
 
-#getpath or filename
-FIND="find ${SVN_PATH} -type f -iname *.sql"
-EXEC="${PSQL_BIN} -h ${PSQL_HOST} -p ${PSQL_PORT} -U ${PSQL_USER} -L ${LOG_PATH}"
+if [ ! -d ${SVN_PATH} ]; then
+    svn co --username ${SVN_USER} --password "${SVN_PWD}" \
+       ${SVN_ADDR} ${SVN_PATH} >> ${LOG_PATH}/svn.log
+else
+    svn up ${SVN_PATH} >> ${SVN_LOG}
+fi
 
-
-#psql -h $host -p 5432 -U $user "password=$pwd" -c 'select datname -L psq.log -o pst.log from pg_database;'
+# Getpath or filename
+cd ${SVN_PATH}
+FIND="find ${SVN_PATH}/${DATE} -type f -iname *.sql"
+EXEC="${PSQL_BIN} -h ${PSQL_HOST} -p ${PSQL_PORT} -U ${PSQL_USER} \
+    password=${PSQL_PASSWD} -L ${LOG_PATH}"
+# Auto input pwd
+#http://blog.163.com/yang_jianli/blog/static/16199000620172134742161?ignoreua
+# View library
+# https://www.cnblogs.com/wzzkaifa/p/6852301.html
+#psql -h $host -p 5432 -U $user "password=$pwd" -c "SELECT datname from pg_database;"
+# View database size
+# https://my.oschina.net/u/347414/blog/544187
+#"SELECT pg_size_pretty(pg_database_size('dataname'));"
 #if [ "`ls -A $SVNPATH`" = "" ]; then
-if [ -d "${SVN_PATH}" ]; then
+
+if [ -d "${SVN_PATH}/${DATA}" ]; then
     # backup sql
+    # http://blog.csdn.net/badly9/article/details/50386259
+    #pg_dump ${DB_NAME} | gzip > ${BACK_PATH}/${DB_NAME}_${DATE}.gz
 
     for SQLIST in `${FIND}`
     do
