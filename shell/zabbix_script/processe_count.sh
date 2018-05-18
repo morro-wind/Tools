4a941574ddea0cbdd9f8b025a96d241ae76a4697
wind <1713931181@qq.com>
2018-05-18 17:28:25 +0800

    modify script

--- shell/zabbix_script/processe_count.sh
+++ shell/zabbix_script/processe_count.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-# Count processes and thread
+# Application processes and thread number counst
 
-PID=``
-ps m -p $PID | wc -l
-ps aux | wc -l
- grep Threads /proc/$PID/status | awk '{print $NF}'
+# Get application pid value, get pid from application listen port
+[[ $1 == thread ]] && PORT=$2 && PID=`netstat -lntp | grep -w ${PORT} | awk '{ print $NF }' \
+    | awk -F "/" '{ print $1 }'`
+
+# When $1 is thread, default TH value is $2,if $2 is null then TH is null
+# The TH value is null, default get all process number
+# If TH value is m, then get all thread in process number
+[[ $1 == process ]] && TH=$2
+
+# Get process or thread in process all
+total_thread() {
+    echo $((`ps aux${TH} | wc -l` - 3))
+}
+
+# Get thread in process
+pro_thread() {
+    echo $(grep Threads /proc/$PID/status | awk '{print $NF}')
+}
+
+case $1 in
+    thread)
+        pro_thread;
+        ;;
+    process)
+        total_thread;
+        ;;
+esac
--- shell/zabbix_script/system_network_connections.sh
+++ shell/zabbix_script/system_network_connections.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Protocol is $1, Value is t or u
+# Protocol is $1, Value is 't' or 'u'
 # STATUS is LISTEN, ESTABLISHED, SYN_SENT, SYN_RECV, FIN_WAIT1, FIN_WAIT2,
 # TIME_WAIT, CLOSE, CLOSE_WAIT, LAST_ACK, LISTEN, CLOSING, UNKNOWN
 # Default PROTOCOL
@@ -9,4 +9,10 @@ PRO=
 [[ ! -z $1 ]] && STATUS=$1 && GREP=grep
 [[ ! -z $2 ]] && PRO=$2
 
-echo $(netstat -ano${PRO} | $GREP $STATUS | wc -l)
+echo $(netstat -ano${PRO} | grep '$STATUS' | wc -l)
+EXC='netstat -ano${PRO} | grep $STATUS | wc -l'
+EXEC=netstat
+ARG=ano
+netstat_tcp_close() {
+    $EXEC -${ARG}t
+echo $($EXC)
