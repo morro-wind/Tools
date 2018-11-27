556ba68e1645e8659f48d9254964bff5b51afa0e
wind <1713931181@qq.com>
2018-11-27 13:51:03 +0800

    Modify icmp add mdev

--- shell/zabbix_script/icmp.sh
+++ shell/zabbix_script/icmp.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 COUNT=$2
 IP=$3
+PACKETSIZE=$4
 
-[[ $# != 3 ]] && echo "$0 args count ip" && exit 2
+[[ $# != 4 ]] && echo "$0 args count ip" && exit 2
 icmpping() {
     ping -c $COUNT $IP > /dev/null
     status=$?
@@ -11,11 +12,15 @@ icmpping() {
 }
 
 icmploss() {
-    ping $IP -c $COUNT | grep loss | awk -F% '{print $1}' | awk '{print $NF}'
+    ping $IP -c $COUNT -s $PACKETSIZE | grep loss | awk -F% '{print $1}' | awk '{print $NF}'
 }
 
 icmptime() {
-    ping $IP -c $COUNT | grep avg | awk -F / '{print $5}'
+    ping $IP -c $COUNT -s $PACKETSIZE | grep avg | awk -F / '{print $5}'
+}
+
+icmpmdev() {
+    ping $IP -c $COUNT -s $PACKETSIZE | grep mdev | awk -F / '{print $7}' | awk '{print $1}'
 }
 
 case $1 in
@@ -27,5 +32,8 @@ case $1 in
         ;;
     time)
         icmptime;
+        ;;
+    mdev)
+        icmpmdev;
         ;;
 esac
