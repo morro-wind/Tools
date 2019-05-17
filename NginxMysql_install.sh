71c922de533d97e7cda2a47d2cad1939bd7602bf
wind <1713931181@qq.com>
2019-05-17 13:28:18 +0800

    update nginx repo

--- NginxMysql_install.sh
+++ NginxMysql_install.sh
@@ -10,14 +10,14 @@ sudo yum install yum-utils
 sudo cat > /etc/yum.repos.d/nginx.repo <<EOF
 [nginx-stable]
 name=nginx stable repo
-baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/centos/6/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
 
 [nginx-mainline]
 name=nginx mainline repo
-baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/mainline/centos/6/x86_64/
 gpgcheck=1
 enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
