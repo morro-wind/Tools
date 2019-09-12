3c2c3dc2e5fcc8933226a47877e4ec64e48a7d39
wind <1713931181@qq.com>
2019-09-12 17:28:07 +0800

    Optimize Document_monitoring_notificatio

--- python/isfile_mail.py
+++ python/isfile_mail.py
@@ -1,12 +1,36 @@
 #!/bin/env python3
 from pathlib import Path
 from pathlib import PurePosixPath
+from subprocess import Popen
 
-p = Path('/home/wind')
-l = list(p.glob('*.sh'))
+file_root = Path('/home/wind')
+list_file = list(file_root.glob('*.sh'))
+app_name = None
+topic_arn = arn:aws:sns:ap-southeast-1:594115287305:ALERMNopro
 
-if l:
-    for i in l:
-       APP = PurePosixPath(i).stem
-       print(APP)
+class DumpFileAlarm:
 
+    def find_file(self):
+        if list_file:
+            for i in list_file:
+                app_name = PurePosixPath(i).stem
+                s = self.send_alarm(app_name)
+                print(s + " " + app_name)
+                self.restart_application(app_name)
+                self.send_alarm(app_name)
+                self.zip_file(app_name)
+
+    def send_alarm(self, app_name, **started):
+        Popen('aws sns publish --topic-arn %s --message
+                "%s" --subject "test appname"' %(topic_arn,p),  shell=True)
+        p = app_name
+        return p
+
+    def restart_application(self, app_name):
+        pass
+
+    def zip_file(self, app_name):
+        pass
+
+if __name__=='__main__':
+    DumpFileAlarm().find_file()
